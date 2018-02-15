12fd96c78e07fec44415ccc7d83b518792264efc
JP Bouchard <bouchard.91@gmail.com>
2018-02-14 19:01:17 -0800

    first commit

---
+++ jp.bat
@@ -0,0 +1,5 @@
+cd "C:\Program Files (x86)\Minecraft\overviewer-0.12.204"
+overviewer.exe C:\Users\bouch\AppData\Roaming\.minecraft\saves\JP C:\Users\bouch\Desktop\JP
+cd "C:\Users\bouch\Desktop\JP"
+rename index.html %date%.html
+start %date%.html
